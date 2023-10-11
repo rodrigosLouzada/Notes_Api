73188baee4621adb187c2652d1b3d8c0a0b1e255
rodrigo <rodrigolouzada32@gmail.com>
2023-10-11 19:17:47 -0300

    knexfile.js,connection between database and knex, creation of table using knex

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
-const path = require("path")
+const path = require("path");
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: (__dirname, "src" , "database" , "database.db")
+      filename: path.resolve(__dirname, "src" , "database" , "database.db")
     },
-    migration: {
-      directory: path.resolve(__dirname, "src" ,"database" , "knex" , "migrationd")
-    }
-  }
-
+    migrations: {
+      directory: path.resolve(__dirname, "src" ,"database" , "knex" , "migrations")
+    },
+    useNullAsDefault: true
+  },
 };
