46dd2aff8e10fbce214e8342ca5c4be91f74d5b8
rodrigo <rodrigolouzada.dev@gmail.com>
2023-11-01 10:57:25 -0300

    describe-jest

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,17 +1,25 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 
-it("user should be create", async () => {
-    const user = {
-        name : "user test" ,
-        email : "user@test.com" ,
-        password : "123"
-    };
+describe("UserCreateService" , () => {
 
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
-    const userCreated =  await userCreateService.execute(user);
-
-    expect(userCreated).toHaveProperty("id");
+    it("user should be create", async () => {
+        const user = {
+            name : "user test" ,
+            email : "user@test.com" ,
+            password : "123"
+        };
     
-});
+        const userRepositoryInMemory = new UserRepositoryInMemory()
+        const userCreateService = new UserCreateService(userRepositoryInMemory);
+        const userCreated =  await userCreateService.execute(user);
+    
+        expect(userCreated).toHaveProperty("id");
+        
+    });
+
+    it("another test" , () => {
+        expect(1).toBe(1)
+    })
+})
+
